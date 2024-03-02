c3854b8b5cab01631651c63e65ccc95014086975
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-03 01:22:53 +0500

    update

--- Question09/app.ts
+++ Question09/app.ts
@@ -1,5 +1,6 @@
 //Favorite Number: Store your favorite number in a variable. 
 //Then, using that variable, create a message that reveals your favorite number. 
 //Print that message.
+
 let favoritenumber: number = 6;
 console.log(`My favorite number is ${favoritenumber}`);
