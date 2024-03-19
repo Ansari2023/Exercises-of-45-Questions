8f225737ca24d42f9c84c02e7d6f497732f16be4
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-19 20:53:18 +0500

    update
    adding

--- Question07and08/app.ts
+++ Question07and08/app.ts
@@ -2,7 +2,7 @@
 //each result in the number 8. Be sure to enclose your operations in print statements 
 //to see the results.
 
-console.log(5+3);
-console.log(11-3);
-console.log(4*2);
-console.log(16/2);
+console.log(5+3); //Addition
+console.log(11-3); //Subtraction
+console.log(4*2); //Multiplication
+console.log(16/2); //Division
